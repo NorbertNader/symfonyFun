7ffb662c3a394c364d23af130b057e787ac6f363
NorbertNader <norbert.nader@gmail.com>
2017-07-06 14:48:13 +0200

    ypui! some symfony fun with htaccess :sunny:

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: norbertnader
+ * Date: 06.07.2017
+ * Time: 14:29
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
